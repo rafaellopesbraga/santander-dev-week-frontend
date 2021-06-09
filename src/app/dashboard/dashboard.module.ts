41d285ddbbd1c913d2001e4f41323922ce8dbe49
Rafael Lopes Braga <rafaellopesbraga@gmail.com>
2021-06-09 14:59:55 -0300

    Create dashboard and populate it with stockcards

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,19 @@
+import { HttpClientModule } from  '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardPageComponent
+    DashboardPageComponent,
+    StockCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
   ],
 })
 export class DashboardModule { }
